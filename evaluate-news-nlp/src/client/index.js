6529a068b3e4abe2f611ec76601e67891f74a6f1
OliverPo666 <55895574+OliverPo666@users.noreply.github.com>
2022-07-30 02:16:38 -0400

    some bugs I dont know how to fix

--- evaluate-news-nlp/src/client/index.js
+++ evaluate-news-nlp/src/client/index.js
@@ -1,17 +1,16 @@
-import { checkForName } from './js/nameChecker'
+import { checkForURL } from './js/urlChecker'
 import { handleSubmit } from './js/formHandler'
+
+// alert("I EXIST")
+// console.log("CHANGE!!");
+
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
-
-export{
-    checkForName,
-    handleSubmit,
-}
+export {
+    checkForURL,
+    handleSubmit
+   }
--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,43 @@
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('url').value
 
+    if(Client.checkForURL(formText)) {
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+
+    postData('http://localhost:8000/api', {url: formText})
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('polarity').innerHTML = `Polarity: '${res.score_tag}`;
+        document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
+        document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+        document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
+        document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
     })
-}
+    } else {
+        alert('Seems like an invalid URL, please try with a valid URL.');
+    }
+};
+
+const postData = async (url = "", data = {}) => {
+    console.log('Analyzing:', data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const newData = await response.json();
+        console.log('Data received:', newData)
+        return newData;
+    } catch (error) {
+        console.log('error', error);
+    }
+};
 
 export { handleSubmit }
